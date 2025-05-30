f81b69149a7166e8ab4a4f5a138ea8680d4937d1
skinnyK <skinnykdev@gmail.com>
2025-05-31 00:04:11 +0200

    - initial setup of project - cleaned out builderplate and unnecessary images, code, etc - added tailwind config file to encompass theme styling

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['Arial', 'Helvetica', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
