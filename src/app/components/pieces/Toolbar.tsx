d3cb60734fce81cfb0c3dfd63a177b0f2c4734a7
skinnyK <skinnykdev@gmail.com>
2025-06-02 14:51:30 +0200

    - began work on search input. - added state variable to hold search text input. using state for testing. implementing query tomorrow

--- src/app/components/pieces/Toolbar.tsx
+++ src/app/components/pieces/Toolbar.tsx
@@ -1,13 +1,25 @@
+"use client";
+
+import { useState } from 'react';
 import Wrapper from "../layout/Wrapper.tsx";
 
 const Toolbar = () => {
+  const [searchText, setSearchText] = useState('');
+
   return (
     <Wrapper>
-      <div className="px-[48px]">
-        <button className="flex gap-4 hover:opacity-80 text-primary-400 font-medium border border-primary-400 px-[18px] py-[8px] rounded-full cursor-pointer transition-colors duration-150">
+      <div className="flex align-center gap-[24px] px-[48px]">
+        <button className="flex gap-4 hover:opacity-80 text-primary-400 font-medium border border-primary-400 px-[18px] py-[8px] rounded-full cursor-pointer transition-colors duration-150" aria-label="add new post">
           <span className="scale-175 origin-center relative -top-[3px]">+</span>
           <span>New Message</span>
         </button>
+        <div className='relative bg-gray-100 py-[8px] px-[24px] rounded-full' aria-label="search through posts">
+          <input type="text" placeholder='Search' className='peer placeholder:px-[20px] focus:placeholder:opacity-0 outline-none' maxLength={32} />
+          <img className='absolute top-[12px] left-[12px] peer-focus:opacity-0' src="./assets/search.png" />
+        </div>
+        <div className='flex'>
+          <span className="align-middle">Date</span>
+        </div>
       </div>
     </Wrapper>
   )
