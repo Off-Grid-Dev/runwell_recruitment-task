e3d3533b72bf4231b63d4bb32579110d10a135fc
skinnyK <skinnykdev@gmail.com>
2025-06-02 12:06:59 +0200

    added content for Headline in Header

--- src/app/components/pieces/Headline.tsx
+++ src/app/components/pieces/Headline.tsx
@@ -3,7 +3,9 @@ import Wrapper from '../layout/Wrapper.tsx'
 const Headline = () => {
   return (
     <Wrapper>
-      <h1>Messages</h1>
+      <div className='px-[48px] pt-[32px] pb-[12px]'>
+        <h1 className='text-3xl leading-9 font-semibold'>Messages</h1>
+      </div>
     </Wrapper>
   )
 }
