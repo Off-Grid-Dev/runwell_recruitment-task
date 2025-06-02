9a19cd04a42864e479bde5cc2a3eea2e348b712f
skinnyK <skinnykdev@gmail.com>
2025-06-02 12:07:16 +0200

    added first button to Toolbar in header

--- src/app/components/pieces/Toolbar.tsx
+++ src/app/components/pieces/Toolbar.tsx
@@ -3,7 +3,12 @@ import Wrapper from "../layout/Wrapper.tsx";
 const Toolbar = () => {
   return (
     <Wrapper>
-      <span>Toolbar</span>
+      <div className="px-[48px]">
+        <button className="flex gap-4 hover:opacity-80 text-primary-400 font-medium border border-primary-400 px-[18px] py-[8px] rounded-full cursor-pointer transition-colors duration-150">
+          <span className="scale-175 origin-center relative -top-[3px]">+</span>
+          <span>New Message</span>
+        </button>
+      </div>
     </Wrapper>
   )
 }
