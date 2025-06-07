19217ae11b5fc40ebbf531878303c19ec0dac7f2
skinnyK <skinnykdev@gmail.com>
2025-06-07 14:24:23 +0200

    added styling to post card component

--- src/app/components/posts/Post.tsx
+++ src/app/components/posts/Post.tsx
@@ -16,9 +16,10 @@ interface PostProps {
 
 const Post: FC<PostProps> = ({ postData }) => {
   return (
-    <div>
-      <h2>{postData.title}</h2>
-      <p>{postData.dateCreated}({postData.timeCreated}){postData.edit && ` Edited: ${postData.dateEdit}(${postData.timeEdit})`}</p>
+    <div className='flex flex-col justify-start mr-auto w-[450px] py-[12px] px-[16px] rounded-[20px] border border-gray-200'>
+      <h2 className='text-2xl font-semibold'>{postData.title}</h2>
+      <p className='text-sm'>{postData.dateCreated}({postData.timeCreated})
+        <span className='text-primary-400'>{postData.edit && ` Edited: ${postData.dateEdit}(${postData.timeEdit})`}</span></p>
       <div>
         {Array.isArray(postData.content)
           ? postData.content.map((line, idx) => <p key={idx}>{line}</p>)
