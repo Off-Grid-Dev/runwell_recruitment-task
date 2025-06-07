3057341c9623b166767b5c94ab40a4f100eb543a
skinnyK <skinnykdev@gmail.com>
2025-06-07 14:23:01 +0200

    wrapped main component to

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react';
+import Wrapper from './layout/Wrapper';
 
 type Props = { children: ReactNode }
 
 const Main = ({ children }: Props) => {
   return (
-    <main>{children}</main>
+    <Wrapper >
+      <main className='grid gap-[24px] my-[24]'>{children}</main>
+    </Wrapper>
   )
 }
 
