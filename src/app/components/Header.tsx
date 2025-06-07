99bc33b8465b797e96ffb54b879f01a990ea9823
skinnyK <skinnykdev@gmail.com>
2025-06-07 14:58:47 +0200

    added new post functionality as prop to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,20 @@
 import TopNav from './header/TopNav.tsx';
 import Headline from './header/Headline.tsx';
 import Toolbar from './header/Toolbar.tsx';
+import React from 'react';
 
-const Header = () => {
+interface HeaderProps {
+  onNewPost: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ onNewPost }) => {
   return (
     <header>
       <TopNav />
       <Headline />
-      <Toolbar />
+      <Toolbar onNewPost={onNewPost} />
     </header>
-  )
-}
+  );
+};
 
 export default Header;
